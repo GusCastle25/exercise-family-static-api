1fc9c5cfc60cdce06e54b2529e4df7a6a794f48f
GusCastle25 <157417128+GusCastle25@users.noreply.github.com>
2024-05-22 10:29:33 +0000

    GusCastle25

--- src/app.py
+++ src/app.py
@@ -6,6 +6,7 @@ from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from datastructures import FamilyStructure
+
 #from models import Person
 
 app = Flask(__name__)
@@ -26,17 +27,35 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
+def get_members():
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
+    return jsonify(members), 200
+
+@app.route('/member/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(id)
+    return jsonify(member), 200  
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    body = request.get_json()
+    new_member = {
+        "id": body["id"],
+        "first_name": body["first_name"],
+        "age": body["age"],
+        "lucky_numbers": body["lucky_numbers"]
     }
-
-
+    jackson_family.add_member(new_member)
+    response_body =  {
+        "message": "Member created",
+        "member" : new_member 
+    }
     return jsonify(response_body), 200
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    member = jackson_family.delete_member(id)
+    return jsonify({"done" : True, "Member deleted": member}), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
